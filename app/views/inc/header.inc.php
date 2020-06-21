788527a1060635278b188a60ca8661c01a5daf2d
davitron2 <49047001+davitron2@users.noreply.github.com>
2020-06-20 19:54:22 -0600

    hhh

--- app/views/inc/header.inc.php
+++ app/views/inc/header.inc.php
@@ -1,3 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!--ZONA FA-->
+    <link rel="stylesheet" href="<?php echo RUTA_URL ?>/css/solid.css">
+    <link rel="stylesheet" href="<?php echo RUTA_URL ?>/css/fontawesome.css">
+<!--ZONA BS-->
+    <link rel="stylesheet" href="<?php echo RUTA_URL ?>/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo RUTA_URL ?>/css/estilos.css">
+    <link rel="stylesheet" href="<?php echo RUTA_URL ?>/css/custom.css">
+    <link rel="stylesheet" href="<?php echo RUTA_URL ?>/css/navbars.css">
+<!--**************************************************************-->
+    <title><?php echo NOMBRE_SITIO;?></title>
+
+    <style>
+        .aqua{background-color: aqua;}
+    </style>
+</head>
+<body>
+
+
 <div class="container container-main-navbar col-sm-12 d-flex justify-content-center flex-column">
 	<div class="container-main-navbar-info">
 		Instituto Tecnol&oacute;gico de Delicias
