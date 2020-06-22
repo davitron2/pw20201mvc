fe702e1c5a18c7f4317dcba8f2f27bd7abd79279
davitron2 <49047001+davitron2@users.noreply.github.com>
2020-06-22 14:52:40 -0600

    l

--- app/views/pages/aulas/agregar.php
+++ app/views/pages/aulas/agregar.php
@@ -25,7 +25,7 @@ include RUTA_APP . '/views/inc/header.inc.php'; ?>
                
                 <div class="row">
                     <div class="col ">
-                        <button type="submit" class="btn btn-success">Guardar <i class="fas fa-save"></i></button>
+                    <button type="submit" class="btn btn-success">Guardar <i class="text-white fas fa-save"></i></button>
                     </div>
                 </div>
             </form>
--- app/views/pages/personal/agregar.php
+++ app/views/pages/personal/agregar.php
@@ -51,7 +51,7 @@ include RUTA_APP . '/views/inc/header.inc.php'; ?>
                 </div>
                 <div class="row">
                     <div class="col text-center">
-                        <button type="submit" class="btn btn-success">Guardar <i class="fas fa-save"></i></button>
+                    <button type="submit" class="btn btn-success">Guardar <i class="text-white fas fa-save"></i></button>
                     </div>
                 </div>
             </form>
