80f032fd64a53ac8dede77655f7908c4281a3309
Alexis Ozaeta <enriqueozaetam@gmail.com>
2020-06-28 16:45:07 -0600

    lista de alumnos por grupo

---
+++ app/views/pages/docentes/grupo.php
@@ -0,0 +1,24 @@
+<?php include RUTA_APP . '/views/inc/header.inc.php'; ?>
+<h5 class="titulo-pagina">Lista de alumnos</h5>
+<div class="container">
+    <div class="table-responsive">
+        <table class="table table-striped table-sm table-bordered tabla-horario">
+            <thead>
+                <tr>
+                    <th scope="col">N&uacute;mero de Control</th>
+                    <th scope="col">Alumno</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                foreach($datos['alumnos'] as $alumno): ?>
+                    <tr>
+                        <td><?php echo $alumno['noControl']; ?></td>
+                        <td><?php echo $alumno['Alumno']; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+<?php include RUTA_APP . '/views/inc/footer.inc.php'; ?>
