2e933e0993b99cd2b55ebe270736a3ece02b2428
Alexis Ozaeta <enriqueozaetam@gmail.com>
2020-06-27 18:04:15 -0600

    diseño de la tablas para horario, parciales y boleta.

---
+++ app/views/pages/alumnos/boleta.php
@@ -0,0 +1,47 @@
+<h5 class="titulo-pagina">Boleta</h5>
+<div class="table-responsive">
+    <table class="table table-bordered table-sm tabla-info-alumno">
+        <thead>
+            <th scope="col">No. Control</th>
+            <th scope="col">Nombre del Alumno</th>
+            <th scope="col">Semestre</th>
+            <th scope="col">Periodo Escolar</th>
+            <th scope="col">Prom. Acum.</th>
+        </thead>
+        <tbody>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tbody>
+    </table>
+    <table class="table table-bordered table-sm tabla-info-alumno">
+        <thead>
+            <th scope="col">Carrera</th>
+        </thead>
+        <tbody>
+            <td></td>
+        </tbody>
+    </table>
+    <table class="table table-bordered table-striped tabla-horario">
+        <thead>
+            <tr>
+                <th scope="col">Materia</th>
+                <th scope="col">Cr</th>
+                <th scope="col">Calificaci&oacute;n</th>
+                <th scope="col">Tipo Evaluaci&oacute;n</th>
+                <th scope="col">Observaciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row"></th> <!-- Materia -->
+                <td></td> <!-- Creditos -->
+                <td></td> <!-- Calificacion -->
+                <td></td> <!-- Tipo evaluacion -->
+                <td></td> <!-- Observaciones -->
+            </tr>
+        </tbody>
+    </table>
+</div> 
---
+++ app/views/pages/alumnos/parciales.php
@@ -0,0 +1,46 @@
+<h5 class="titulo-pagina">Calificaciones Parciales</h5>
+<div class="table-responsive">
+    <table class="table table-bordered table-sm tabla-info-alumno">
+        <thead>
+            <th scope="col">No. Control</th>
+            <th scope="col">Nombre del Alumno</th>
+            <th scope="col">Periodo Escolar</th>
+        </thead>
+        <tbody>
+            <td></td> <!-- Numero de control -->
+            <td></td> <!-- Nombre -->
+            <td></td> <!-- Periodo escolar -->
+        </tbody>
+    </table>
+    <table class="table table-bordered table-sm tabla-parciales">
+        <thead>
+            <tr>
+                <th scope="col" rowspan="2">Materia</th>
+                <th scope="col" rowspan="2">Grupo</th>
+                <th scope="col" colspan="7">Unidades</th>    
+            </tr>
+            <tr>
+                <th scope="col">1</th>
+                <th scope="col">2</th>
+                <th scope="col">3</th>
+                <th scope="col">4</th>
+                <th scope="col">5</th>
+                <th scope="col">6</th>
+                <th scope="col">7</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr> 
+                <td></td> <!-- Materia  -->
+                <td></td> <!-- Grupo -->
+                <td></td> <!-- 1 -->
+                <td></td> <!-- 2 -->
+                <td></td> <!-- 3 -->
+                <td></td> <!-- 4 -->
+                <td></td> <!-- 5 -->
+                <td></td> <!-- 6 -->
+                <td></td> <!-- 7 -->
+            </tr>
+        </tbody>
+    </table>
+</div>
